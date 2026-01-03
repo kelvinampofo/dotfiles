a1c80610db0f791e3ef70c28750663ade5592467
Kelvin Ampofo <kelvnamp@gmail.com>
2026-01-03 11:21:51 +0000

    Update dotfiles

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt
-        set -l time_str (date "+%H:%M:%S %Z")
+        set -l time_str (date "+%H:%M:%S")
         set_color a98ad3
         printf '%s ' $time_str
         set_color normal
